cad6b3dac722941abcdcd321eb18072f5f290dbe
Shawnjoseph2001 <shawnjoseph2001@gmail.com>
2023-08-03 00:25:30 -0700

    made changes

--- project4/network/views.py
+++ project4/network/views.py
@@ -1,9 +1,12 @@
+import datetime
 import json
+import time
 
 import requests
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.syndication.views import Feed
 from django.core.paginator import Paginator, Page
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect
@@ -30,150 +33,54 @@ def superuser_check(user_check):
 
 def index(request):
     """Redirect to the 'all_rd' route."""
-    return HttpResponseRedirect(reverse("all_rd"))
+    return HttpResponseRedirect(reverse("all"))
 
 
 @login_required
 def following(request, page_num):
+    return render_index(request, "following", following_only=True, page_num=page_num)
+
+
+def render_index(
+    request, page_view_name, posts_contains=None, following_only=False, page_num=1
+):
     """
     Fetches and displays all posts in the network, ordered by timestamp.
 
     This function also handles pagination and provides information about liked posts and likes.
     """
-    local_server = get_object_or_404(ForeignServer, ip="local")
-
-    follow_list = Follower.objects.filter(
-        followee_user=request.user.username
-    ).values_list("followee_user", flat=True)
-    following_users = list(follow_list.values())
-    local_following_users = Follower.objects.filter(
-        following_user=request.user, server=local_server
-    ).values_list("followee_user", flat=True)
-    blocked_servers = ForeignBlocklist.objects.filter(user=request.user).values_list(
-        "server", flat=True
-    )
-
-    # Query all posts and handle pagination
-    # Count likes for each post
-    post_list = list(
-        Post.objects.filter(user__username__in=list(local_following_users))
-        .order_by("-timestamp")
-        .values()
-    )
-    for i in post_list:
-        i["server_id"] = str(local_server.id)
-        i["likes"] = len(ForeignLike.objects.filter(post__id=i["id"]))
-        i["comments"] = list(ForeignComment.objects.filter(post__id=i["id"]).values())
-        for j in i["comments"]:
-            j["username"] = str(get_object_or_404(User, id=j["user_id"]).username)
-        i["username"] = str(get_object_or_404(User, id=i["user_id"]).username)
-        i["server_name"] = "local"
-        i["server_port"] = ""
-        i["liked"] = ForeignLike.objects.filter(
-            user=request.user, server=local_server, post__id=i["id"]
-        ).exists()
-        i["following"] = Follower.objects.filter(
-            following_user=request.user,
-            followee_user=i["username"],
-            server=local_server,
-        ).exists()
-    for i in ForeignServer.objects.all():
-        if i.id not in blocked_servers and i.ip != "local":
-            try:
-                result = requests.get(
-                    "http://" + i.ip + ":" + str(i.port) + "/federation/posts",
-                    timeout=5,
-                    data=json.dumps(
-                        {
-                            "username": request.user.username,
-                            "port": request.META["SERVER_PORT"],
-                        }
-                    ),
-                )
-                if result.status_code == 200:
-                    json_data = json.loads(result.content)
-                    remotePosts = []
-                    for j in json_data["posts"]:
-                        if Follower.objects.filter(
-                            following_user=request.user,
-                            followee_user=j["username"],
-                            server=i,
-                        ).exists():
-                            j["server_name"] = str(i.ip)
-                            j["server_port"] = str(i.port)
-                            j["server_id"] = str(i.id)
-                            j["following"] = True
-                            remotePosts.append(j)
-                    post_list += remotePosts
-            except:
-                pass
-
-    paginator = Paginator(post_list, 10)
-    posts: Page = paginator.get_page(page_num)
-
-    # Handle pagination
-    next_page = "0"
-    prev_page = "0"
-    if page_num > 1:
-        prev_page = reverse("all", args=(page_num - 1,))
-    if page_num < paginator.num_pages:
-        next_page = reverse("all", args=(page_num + 1,))
-
-    # Render the page
-    return render(
-        request,
-        "network/index.html",
-        {
-            "posts": posts,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "following_users": following_users,
-            "server_id": str(get_object_or_404(ForeignServer, ip="local").id),
-        },
-    )
-
-
-def all_rd(request):
-    """Redirect to the first page of the 'all_rd' route."""
-    return HttpResponseRedirect(reverse("all_rd") + "/1")
-
-
-def follow_rd(request):
-    """Redirect to the first page of the 'following' route."""
-    return HttpResponseRedirect(reverse("following") + "/1")
-
-
-def all_posts(request, page_num):
-    """
-    Fetches and displays all posts in the network, ordered by timestamp.
-
-    This function also handles pagination and provides information about liked posts and likes.
-    """
-    following_users = []
     blocked_servers = []
-    blocked_users = []
     if request.user.is_authenticated:
-        following_users = list(
-            Follower.objects.filter(followee_user=request.user.username)
-            .values_list("followee_user", flat=True)
-            .values()
-        )
         blocked_servers = ForeignBlocklist.objects.filter(
             user=request.user
         ).values_list("server", flat=True)
-
     # Query all posts and handle pagination
     local_server = get_object_or_404(ForeignServer, ip="local")
     # Count likes for each post
-    initial_post_list = list(Post.objects.filter().order_by("-timestamp").values())
+    initial_post_list = list(Post.objects.order_by("-timestamp").values())
     post_list = []
     for i in initial_post_list:
         i["username"] = str(get_object_or_404(User, id=i["user_id"]).username)
         if (
             request.user.is_authenticated
-            and ForeignUserBlocklist.objects.filter(
-                user=request.user, server=local_server, blocked_user=i["username"]
-            ).exists()
+            and (
+                (
+                    ForeignUserBlocklist.objects.filter(
+                        user=request.user,
+                        server=local_server,
+                        blocked_user=i["username"],
+                    ).exists()
+                )
+                or (
+                    following_only
+                    and not Follower.objects.filter(
+                        following_user=request.user,
+                        server=local_server,
+                        followee_user=i["username"],
+                    ).exists()
+                )
+            )
+            or (posts_contains is not None and posts_contains not in str(i))
         ):
             continue
         i["server_id"] = str(local_server.id)
@@ -181,6 +88,16 @@ def all_posts(request, page_num):
         i["comments"] = list(ForeignComment.objects.filter(post__id=i["id"]).values())
         i["server_name"] = "local"
         i["server_port"] = ""
+        i["timestamp_user"] = (
+            i["timestamp"]
+            .astimezone(datetime.timezone.utc)
+            .strftime("%b. %d, %Y, %I:%M %p")
+        )
+        i["timestamp"] = (
+            i["timestamp"]
+            .replace(tzinfo=datetime.timezone.utc)
+            .strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        )
         i["liked"] = ForeignLike.objects.filter(
             user=request.user, server=local_server, post__id=i["id"]
         ).exists()
@@ -213,34 +130,57 @@ def all_posts(request, page_num):
                         j["server_name"] = str(i.ip)
                         j["server_port"] = str(i.port)
                         j["server_id"] = str(i.id)
-                        if request.user.is_authenticated:
-                            j["following"] = Follower.objects.filter(
+                        if (
+                            request.user.is_authenticated
+                            and Follower.objects.filter(
                                 following_user=request.user,
                                 followee_user=j["username"],
                                 server=i,
                             ).exists()
-                            if ForeignUserBlocklist.objects.filter(
-                                user=request.user,
-                                server=local_server,
-                                blocked_user=j["username"],
-                            ).exists():
-                                continue
+                        ):
+                            j["following"] = True
+                        else:
+                            j["following"] = False
+                        if (
+                            request.user.is_authenticated
+                            and (
+                                ForeignUserBlocklist.objects.filter(
+                                    user=request.user,
+                                    server=local_server,
+                                    blocked_user=j["username"],
+                                ).exists()
+                                or (following_only and not j["following"])
+                            )
+                            or (
+                                posts_contains is not None
+                                and posts_contains not in str(i)
+                            )
+                        ):
+                            continue
                         append_posts.append(j)
                     post_list += append_posts
             except Exception as e:
                 print(e)
                 pass
-
-    paginator = Paginator(post_list, 10)
+    sorted_post_list = sorted(
+        post_list,
+        key=lambda x: int(
+            time.mktime(
+                datetime.datetime.strptime(
+                    x["timestamp"], "%Y-%m-%dT%H:%M:%S.%fZ"
+                ).utctimetuple()
+            )
+        ),
+    )
+    paginator = Paginator(sorted_post_list, 10)
     posts: Page = paginator.get_page(page_num)
-
     # Handle pagination
     next_page = "0"
     prev_page = "0"
     if page_num > 1:
-        prev_page = reverse("all", args=(page_num - 1,))
+        prev_page = reverse(page_view_name, args=(page_num - 1,))
     if page_num < paginator.num_pages:
-        next_page = reverse("all", args=(page_num + 1,))
+        next_page = reverse(page_view_name, args=(page_num + 1,))
 
     # Render the page
     return render(
@@ -250,10 +190,13 @@ def all_posts(request, page_num):
             "posts": posts,
             "next_page": next_page,
             "prev_page": prev_page,
-            "following_users": following_users,
             "server_id": str(get_object_or_404(ForeignServer, ip="local").id),
         },
     )
+
+
+def all_posts(request, page_num=1):
+    return render_index(request, page_view_name="all", page_num=page_num)
 
 
 def login_view(request):
@@ -315,16 +258,44 @@ def post(request):
     Creates a new post associated with the current user and redirects to the index page.
     """
     if request.method == "POST":
-        Post.objects.create(content=request.POST["content"], user=request.user)
+        content_str = request.POST["content"]
+        if "#" in content_str:
+            new_string = ""
+            for i in request.POST["content"].split(" "):
+                if i.startswith("#"):
+                    new_string += (
+                        '<a href="'
+                        + reverse("search")
+                        + "?q=%23"
+                        + i[1:]
+                        + '">'
+                        + i
+                        + "</a> "
+                    )
+                else:
+                    new_string += i + " "
+            content_str = new_string
+        content_str = content_str.replace(
+            "/!",
+            '<p style="background-color: #000; color: #000;" '
+            "onmouseover=\"this.style.color='#FFF';\" "
+            "onmouseout=\"this.style.color='#000';\">",
+        )
+        content_str = content_str.replace("!/", "</p>")
+        Post.objects.create(content=content_str, user=request.user)
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/post.html")
 
 
-def user(request, username, server_id, page_num):
+def user(request, username, server_id, page_num=1):
     """Handles requests for a specific user's posts, along with their profile info"""
-    print()
-    server = get_object_or_404(ForeignServer, id=server_id)
+    if server_id == "local":
+        server = get_object_or_404(ForeignServer, ip="local")
+        local_server = server
+    else:
+        server = get_object_or_404(ForeignServer, id=server_id)
+        local_server = get_object_or_404(ForeignServer, ip="local")
     # Initialize variables for authenticated users
     user_following = False
     liked_posts = []
@@ -339,9 +310,7 @@ def user(request, username, server_id, page_num):
                 "post__id", flat=True
             )
         )
-    followers = len(
-        Follower.objects.filter(server__id=server_id, followee_user=username)
-    )
+    followers = len(Follower.objects.filter(server=server, followee_user=username))
     following_users: int
     posts: list
     if server.ip == "local":
@@ -354,12 +323,17 @@ def user(request, username, server_id, page_num):
         )
         # Fetch the count of local followers and following users
         # The server is only aware of followers local to the host and dest servers
-        followers += len(Follower.objects.filter(following_user=request_user))
         following_users = len(Follower.objects.filter(followee_user=request_user))
         # Count likes for each post
         for i in posts:
             i["likes"] = len(ForeignLike.objects.filter(post=i["id"]))
             i["username"] = request_user.username
+            i["comments"] = list(
+                ForeignComment.objects.filter(
+                    user=username, server=server, post=i["id"]
+                ).values()
+            )
+
     else:
         try:
             result = requests.get(
@@ -381,6 +355,7 @@ def user(request, username, server_id, page_num):
     # Handle pagination
     next_page = "0"
     prev_page = "0"
+
     paginator = Paginator(posts, 10)
     if page_num > 1:
         prev_page = reverse(
@@ -415,6 +390,7 @@ def user(request, username, server_id, page_num):
             "liked_posts": liked_posts,
             "server_id": server_id,
             "local_server": server.ip == "local",
+            "not_self": server != local_server or request.user.username != username,
         },
     )
 
@@ -790,6 +766,11 @@ def federated_posts(request):
         i["likes"] = len(ForeignLike.objects.filter(post__id=i["id"]))
         i["comments"] = list(ForeignComment.objects.filter(post__id=i["id"]).values())
         i["username"] = str(get_object_or_404(User, id=i["user_id"]).username)
+        i["timestamp_user"] = (
+            i["timestamp"]
+            .astimezone(datetime.timezone.utc)
+            .strftime("%b. %d, %Y, %I:%M %p")
+        )
         if "username" in json_data:
             i["liked"] = ForeignLike.objects.filter(
                 server=server, user=json_data["username"], post__id=i["id"]
@@ -821,3 +802,33 @@ def unblock_user(request, server_id, username):
         server__id=server_id, blocked_user=username, user=request.user
     ).delete()
     return HttpResponseRedirect(reverse("index"))
+
+
+class RSSFeed(Feed):
+    title = "Network RSS Feed"
+    link = "/"
+    description = "The RSS feed for this Federation instance."
+
+    def items(self):
+        return Post.objects.all()
+
+    def item_title(self, item):
+        return item.user.username
+
+    def item_description(self, item):
+        return item.content
+
+    def item_link(self, item):
+        return reverse("index")
+
+
+def search(request, page_num=1):
+    if "q" in request.GET:
+        return render_index(
+            request,
+            "search?q=" + request.GET["q"],
+            posts_contains=request.GET["q"],
+            page_num=page_num,
+        )
+    else:
+        return HttpResponseRedirect(reverse("index"))
